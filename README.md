d6aa1ba41fd0f37082b83d1ccfef02206b95ac49
92SooJong <hsz3855@gmail.com>
2022-05-27 16:13:01 +0900

    삭제 수정 구현완료

---
+++ README.md
@@ -0,0 +1,2 @@
+## Notion링크
+[https://www.notion.so/1e07b5071f3b4f9fbf78d093a77be903](https://www.notion.so/1e07b5071f3b4f9fbf78d093a77be903)
--- src/main/java/com/jojoldu/book/springboot/web/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,6 +25,12 @@ public class PostsService {
     }
 
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
@@ -35,4 +45,13 @@ public class PostsService {
         entity.update(postsUpdateRequestDto.getTitle(),postsUpdateRequestDto.getContent());
         return id;
     }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+
+    }
+
 }
